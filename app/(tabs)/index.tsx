794c564a5ae13974fcc2272b57461991ba76ca16
haitaa <mustafahaitaa@gmail.com>
2025-01-17 15:36:09 +0200

    setup fonts and assets

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,9 @@ import { Link } from "expo-router";
 export default function Index() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text className="font-bold text-lg my-10">Welcome to HaitaEstate</Text>
+      <Text className="font-bold text-3xl font-rubik my-10">
+        Welcome To HaitaEstate
+      </Text>
       <Link href={"/sign-in"}>Sign In</Link>
       <Link href={"/explore"}>Explore</Link>
       <Link href={"/profile"}>Profile</Link>
