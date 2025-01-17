a6bbc7537bcf473fe2c5a624f13071f08fe88708
haitaa <mustafahaitaa@gmail.com>
2025-01-17 15:19:47 +0200

    nativewind setup

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
+
+export default function Index() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text className="font-bold text-lg my-10">Welcome to HaitaEstate</Text>
+      <Link href={"/sign-in"}>Sign In</Link>
+      <Link href={"/explore"}>Explore</Link>
+      <Link href={"/profile"}>Profile</Link>
+      <Link href={"/properties/1"}>Properties</Link>
+    </View>
+  );
+}
